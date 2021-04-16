b578274d37b4cfbf17b10a9a2b7ef8b5410ec0a2
jason-kerney <pf!PsX87)=S4>
2021-04-16 07:06:28 -0700

    safe-sql

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210415.1756';
+const cacheName = 'chirpy-20210416.0705';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
