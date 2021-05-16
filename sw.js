176f3adbe06bbdfbefb2cc9f2abb938261ecfbd7
jason-kerney <pf!PsX87)=S4>
2021-05-16 11:11:19 -0700

    missing old content & redirects

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210516.0251';
+const cacheName = 'chirpy-20210516.1110';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
