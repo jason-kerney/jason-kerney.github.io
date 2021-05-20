6c09649415c2ba21e2eb04a989f87c29c3d9a0bc
jason-kerney <pf!PsX87)=S4>
2021-05-20 12:45:36 -0700

    fixed spelling error

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210516.1110';
+const cacheName = 'chirpy-20210520.1243';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
