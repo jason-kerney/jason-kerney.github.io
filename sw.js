3e854c207efe5bf572b3bd677d2161e7fc395b7c
jason-kerney <pf!PsX87)=S4>
2021-07-05 19:52:36 -0700

    trying tag again

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210702.0656';
+const cacheName = 'chirpy-20210705.1951';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
