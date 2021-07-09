8bc0b9400660d5c947fd01cf9f95477ca9ecc501
jason-kerney <pf!PsX87)=S4>
2021-07-09 06:25:38 -0700

    publish

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210705.1951';
+const cacheName = 'chirpy-20210709.0624';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
