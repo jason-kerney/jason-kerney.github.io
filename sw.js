57b9891505f861cf2585a9aaf3f4e0f5884d859c
jason-kerney <pf!PsX87)=S4>
2021-07-16 07:13:00 -0700

    added alias

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210716.0651';
+const cacheName = 'chirpy-20210716.0712';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
