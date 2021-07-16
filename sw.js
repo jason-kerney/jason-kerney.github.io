3c3769441661103777bfffa72ea1ccdabe1b411c
jason-kerney <pf!PsX87)=S4>
2021-07-16 06:53:12 -0700

    Oksana

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210709.1453';
+const cacheName = 'chirpy-20210716.0651';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
